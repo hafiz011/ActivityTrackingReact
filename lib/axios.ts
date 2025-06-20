d00f808348ce2b8ecdc7a405193ce0afd73120c6
Hafiz011 <hrhafij8@gmail.com>
2025-06-20 13:03:29 +0600

    update forgot-password

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://localhost:5011/api",
+  baseURL: "http://localhost:5011/api",
   headers: {
     "Content-Type": "application/json",
   },
