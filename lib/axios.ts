320a0dca7248c768be5059415e6458bb1d1b06c7
hafiz011 <hrhafij8@gmail.com>
2025-07-03 15:07:39 +0600

    update micro service routing

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://localhost:5011/api",
+  baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
