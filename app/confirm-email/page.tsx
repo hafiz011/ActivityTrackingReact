5996ad7f55d9165bd19a8ba5b1dec3f67f6946ce
cyber ghost <hrhafij8@gmail.com>
2025-07-06 00:42:57 +0600

    Fix dynamic export error in confirm-email page

--- app/confirm-email/page.tsx
+++ app/confirm-email/page.tsx
@@ -1,4 +1,20 @@
+"use client"
+
 import ConfirmEmailClient from "@/components/auth/ConfirmEmailClient"
+
 export default function ConfirmEmailPage() {
   return <ConfirmEmailClient />
 }
+
+
+// // app/confirm-email/page.tsx
+// import { Suspense } from "react"
+// import ConfirmEmailClient from "@/components/auth/ConfirmEmailClient"
+
+// export default function ConfirmEmailPage() {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <ConfirmEmailClient />
+//     </Suspense>
+//   )
+// }
