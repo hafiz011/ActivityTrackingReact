74962f11456e9a56240870fd5a85433061b77920
hafiz011 <hrhafij8@gmail.com>
2025-07-07 16:08:25 +0600

    fix reset password page

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -10,14 +10,10 @@
 
 
 "use client";
-// /app/reset-password/page.tsx
-import dynamic from "next/dynamic";
 
-// Force disable SSR (prevents Suspense errors)
-const ResetPassword = dynamic(() => import("@/components/auth/ResetPassword"), {
-  ssr: false,
-});
+import ResetPassword from "@/components/auth/ResetPassword";
+export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <ResetPassword />;
+  return <ResetPassword />
 }
