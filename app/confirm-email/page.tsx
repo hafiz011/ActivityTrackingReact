d63b1d8a7210ef1baabdcc394bcb627f8f35ff29
hafiz011 <hrhafij8@gmail.com>
2025-07-07 17:01:59 +0600

    fix searchParams error

--- app/confirm-email/page.tsx
+++ app/confirm-email/page.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 
+import { Suspense } from "react";
 import ConfirmEmailClient from "@/components/auth/ConfirmEmailClient";
 
+export const dynamic = "force-dynamic";
 
-export const dynamic = "force-dynamic"
-
-
-
-export default function ConfirmEmailPage() {
-  return <ConfirmEmailClient />
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ConfirmEmailClient />
+    </Suspense>
+  );
 }
+
 
 // // app/confirm-email/page.tsx
 // import { Suspense } from "react"
--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,21 +1,14 @@
-// // /app/reset-password/page.tsx
-
-// "use client";
-// import ResetPassword from "@/components/auth/ResetPassword";
-// export const dynamic = "force-dynamic"
-
-// export default function Page() {
-//   return <ResetPassword />; // import from your component or inline here
-// }
 
 "use client"
-
+import { Suspense } from "react";
 import ResetPassword from "@/components/auth/ResetPassword";
 
 export const dynamic = "force-dynamic"
 
 export default function Page() {
   return (
-    <ResetPassword />
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPassword />
+    </Suspense>
   );
 }
