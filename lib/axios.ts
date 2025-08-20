9703331107eb07fddc12bb49b189d448f4c01ce5
Md Hafiz <73638850+hafiz011@users.noreply.github.com>
2025-08-20 22:06:09 +0600

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://techciph.com",
   // baseURL: "http://localhost:5000",
+  
   headers: {
     "Content-Type": "application/json",
   },
